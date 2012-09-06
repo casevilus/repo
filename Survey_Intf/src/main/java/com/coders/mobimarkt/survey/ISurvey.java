8de03014550695b397fbcaf3941e6ad79ecceb22
OzkanDemir <ozkandemir80@gmail.com>
2012-09-06 21:51:57 +0300

    Core_Parent first commit...

--- Survey_Intf/src/main/java/com/coders/mobimarkt/survey/ISurvey.java
+++ Survey_Intf/src/main/java/com/coders/mobimarkt/survey/ISurvey.java
@@ -3,6 +3,7 @@ package com.coders.mobimarkt.survey;
 import com.coders.mobimarkt.data.intf.IData;
 import com.coders.mobimarkt.interaction.intf.IInteraction;
 
+
 public interface ISurvey {
 	public IInteraction getInteraction(IData data);
 }
